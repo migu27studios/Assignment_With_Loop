df60183c657c897664c886e076cc51d67e47a0b0
Unknown <migu27studios@gmail.com>
2017-02-24 10:53:58 -0700

    Added For Each Loops and went over the For Loops

---
+++ Assignment_Loops/Assets/ForLoops.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ForLoops : MonoBehaviour {
+
+    //For loops have an iterator
+
+    public List<string> cars;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < cars.Count; i++)   //The middle part if for List.Count  (For lists we use .Count).
+        {
+            print(cars[i]);
+        }   
+	
+	}
+	
+
+}
---
+++ Assignment_Loops/Assets/ListWithLoops.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; //For Lists!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! USE IT!!!!!!!!!!!!!
+public class ListWithLoops : MonoBehaviour {
+
+
+    public List<string> animals;
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        foreach (string item in animals)
+        {
+            if (item == "Cat")
+            {
+                print(item);
+            }
+        }
+	}
+
+}
